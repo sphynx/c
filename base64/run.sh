e4e8643fa2753ae47a447b8f00fc28e058943047
Ivan Veselov <veselov@gmail.com>
2018-10-21 19:29:30 +0100

    Remove run.sh since it is replaced by Makefile

--- base64/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-CFLAGS="-g -Wall -Wextra -W -Werror --pedantic --std=c99 -O2"
-
-gcc $CFLAGS -o my-base64 hex.c base64.c my-base64.c && shelltest . --color -p && ./random_test.sh
-# gcc $CFLAGS -o hex2base64 hex.c base64.c hex2base64.c
