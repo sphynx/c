cb0bb8a994ed477590fb87d0d0eca1f3757b70fb
Ivan Veselov <veselov@gmail.com>
2018-10-22 17:40:45 +0100

    Move xor-related functionality to xor.h. Refactorings

--- base64/xor.c
+++ base64/xor.c
@@ -1,10 +1,28 @@
 #include <assert.h>
-#include <inttypes.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "hex.h"
+#include "xor.h"
+
+/*
+ * Returns bitwise xor of two byte buffers of the same length. The
+ * resulting byte buffer is malloc-ed, it's responsibility of the
+ * caller to free it.
+ */
+uint8_t* xor(uint8_t* b1, uint8_t* b2, size_t len) {
+    uint8_t* res = malloc(len);
+    if (res == NULL) {
+        printf("out of memory\n");
+        return NULL;
+    }
+
+    for (unsigned int i = 0; i < len; i++) {
+        res[i] = b1[i] ^ b2[i];
+    }
+
+    return res;
+}
 
 /*
  * Returns bitwise xor of two buffers represented as hex strings.
@@ -13,23 +31,15 @@
  */
 char* xor_hex(char* s1, char* s2) {
     assert(strlen(s1) == strlen(s2));
+
     size_t hex_len = strlen(s1);
-    size_t byte_len = hex_len / 2;
+    size_t bytes_len = hex_len / 2;
 
     uint8_t* b1 = read_hex(s1, hex_len);
     uint8_t* b2 = read_hex(s2, hex_len);
+    uint8_t* res = xor(b1, b2, bytes_len);
 
-    uint8_t* re = malloc(byte_len);
-    if (re == NULL) {
-        printf("out of memory\n");
-        return NULL;
-    }
-
-    for (unsigned int i = 0; i < byte_len; i++) {
-        re[i] = b1[i] ^ b2[i];
-    }
-
-    return write_hex(re, byte_len);
+    return write_hex(res, bytes_len);
 }
 
 /*
---
+++ base64/xor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdio.h>
+#include <inttypes.h>
+
+/*
+ * Returns bitwise xor of two byte buffers of the same length. The
+ * resulting byte buffer is malloc-ed, it's responsibility of the
+ * caller to free it.
+ */
+uint8_t* xor(uint8_t* b1, uint8_t* b2, size_t len);
+
+/*
+ * Returns bitwise xor of two buffers represented as hex strings.
+ * Result will also be encoded as a hex string. The resulting string
+ * is malloc-ed, it's responsibility of the caller to free it.
+ */
+char* xor_hex(char* s1, char* s2);
