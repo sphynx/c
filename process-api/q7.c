d146d7cb9782af0216532222e02ad4e64033ad6d
Ivan Veselov <veselov@gmail.com>
2018-10-24 22:30:23 +0100

    Fix ex7 answer

--- process-api/q7.c
+++ process-api/q7.c
@@ -7,6 +7,13 @@
  * Answer: It looks like nothing happens, i.e. printf just doesn't
  * print anything if stdout has been closed. I would expect it to
  * throw some kind of error, but it doesn't.
+ *
+ * Follow-up: now I've just re-read my answer above (after reading
+ * about error handling in C in King's book) and realise that I was
+ * dumb. Of course, there will be an error! (I presume it should be
+ * EBADF -- not a valid file descriptor open for writing, because we
+ * boldly closed `stdout`) -- I just didn't check. So now I am
+ * checking and indeed it is EBADF: "Bad file descriptor", bingo!
  */
 
 #include <stdio.h>
@@ -23,7 +30,10 @@ int main(void) {
         // child
         printf("child\n");
         close(STDOUT_FILENO);
-        printf("after closing stdout\n");
+        if (printf("after closing stdout\n")) {
+            perror("printf");
+            exit(1);
+        }
     } else {
         // parent
         printf("parent\n");
