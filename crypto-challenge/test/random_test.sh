2c64f23bcf26a3ea43da5870be9df7547961ec0b
Ivan Veselov <veselov@gmail.com>
2018-10-27 15:09:57 +0100

    Rename my-base64 to encode-base64 to match decoding tool

--- crypto-challenge/test/random_test.sh
+++ crypto-challenge/test/random_test.sh
@@ -1,12 +1,11 @@
 NUM_OF_RUNS=100
 FILE=test.rnd
-BIN=.
 for x in $(seq 1 ${NUM_OF_RUNS})
 do
     size=$(jot -r 1 1 20000)  # random number from 1 to 20k
     dd if=/dev/urandom of=${FILE} bs=${size} count=1 2>/dev/null
     base64 ${FILE} >result.baseline
-    ${BIN}/my-base64 ${FILE} >result.mine
+    ./encode-base64 ${FILE} >result.mine
     if ! diff -q result.baseline result.mine; then
         echo "[FAIL] Test failed"
         echo "Expected (from result.baseline):"
