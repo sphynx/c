7619c216cd2ff27e1d3c2805d34cca77c16d1015
Ivan Veselov <veselov@gmail.com>
2018-11-04 20:41:00 +0000

    Add benchmarking of calls using `gettimeofday` and `clock_gettime(CLOCK_MONOTONIC)

--- measure-system-calls/measure.c
+++ measure-system-calls/measure.c
@@ -1,21 +1,193 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <unistd.h>
 #include <time.h>
 
-int main(void)
+// Returns delta in microseconds (1e-6 s).
+static double
+timeval_delta(struct timeval *start, struct timeval *finish)
+{
+    return (finish->tv_sec - start->tv_sec) * 1e6 +
+        (finish->tv_usec - start->tv_usec);
+}
+
+// Returns delta in nanoseconds (1e-9 s).
+static double
+timespec_delta(struct timespec *start, struct timespec *finish)
+{
+    return (finish->tv_sec - start->tv_sec) * 1e9 +
+        (finish->tv_nsec - start->tv_nsec);
+}
+
+// Returns time per call in nanoseconds.
+static double
+timeval_pace(struct timeval *start, struct timeval *finish, size_t total)
+{
+    return timeval_delta(start, finish) / total * 1000.0;
+}
+
+// Returns time per call in nanoseconds.
+static double
+timespec_pace(struct timespec *start, struct timespec *finish, size_t total)
+{
+    return timespec_delta(start, finish) / total;
+}
+
+static void
+measure_gettimeofday(void)
 {
     struct timeval t[(int)1e6];
     size_t total = sizeof(t) / sizeof(struct timeval);
 
+    printf("Running %zu `gettimeofday` calls...\n", total);
     for (unsigned int i = 0; i < total; i++) {
         gettimeofday(&t[i], NULL);
     }
 
-    printf("%zu calls: from %d to %d\n",
-           total, t[0].tv_usec, t[total-1].tv_usec);
-    double pace = (double) (t[total-1].tv_usec - t[0].tv_usec) / total;
-    printf("%.1f ns per call\n", pace * 1000);
+    double pace = timeval_pace(&t[0], &t[total-1], total);
+    printf("%.1f ns per `gettimeofday` call\n", pace);
+}
 
+static void
+measure_clock_gettime_monotonic(void)
+{
+    struct timespec t[(int)1e6];
+    size_t total = sizeof(t) / sizeof(struct timespec);
+
+    printf("Running %zu `clock_gettime` calls...\n", total);
+    for (unsigned int i = 0; i < total; i++) {
+        clock_gettime(CLOCK_MONOTONIC, &t[i]);
+    }
+
+    double pace = timespec_pace(&t[0], &t[total-1], total);
+    printf("%.1f ns per `clock_gettime(CLOCK_MONOTONIC)` call\n", pace);
+}
+
+static void
+measure_clock_gettime_monotonic_raw(void)
+{
+    struct timespec t[(int)1e6];
+    size_t total = sizeof(t) / sizeof(struct timespec);
+
+    printf("Running %zu `clock_gettime` calls...\n", total);
+    for (unsigned int i = 0; i < total; i++) {
+        clock_gettime(CLOCK_MONOTONIC_RAW, &t[i]);
+    }
+
+    double pace = timespec_pace(&t[0], &t[total-1], total);
+    printf("%.1f ns per `clock_gettime(CLOCK_MONOTONIC_RAW)` call\n", pace);
+}
+
+static void
+measure_read_with_gtod(void)
+{
+    size_t total = (size_t)1e6;
+
+    unsigned char *buf = malloc(1);
+    if (buf == NULL) {
+        exit(1);
+    }
+
+    struct timeval start;
+    struct timeval stop;
+
+    printf("Running %zu `read` calls...\n", total);
+    gettimeofday(&start, NULL);
+    for (unsigned int i = 0; i < total; i++) {
+        read(STDIN_FILENO, buf, 0);
+    }
+    gettimeofday(&stop, NULL);
+
+    double pace = timeval_pace(&start, &stop, total);
+    printf("%.1f ns per `read` call (measured with `gettimeofday`)\n",
+           pace);
+}
+
+static void
+measure_read_with_cgt(void)
+{
+    size_t total = (size_t)1e6;
+
+    unsigned char *buf = malloc(1);
+    if (buf == NULL) {
+        exit(1);
+    }
+
+    struct timespec start;
+    struct timespec stop;
+
+    printf("Running %zu `read` calls...\n", total);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    for (unsigned int i = 0; i < total; i++) {
+        read(STDIN_FILENO, buf, 0);
+    }
+    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+
+    double pace = timespec_pace(&start, &stop, total);
+    printf("%.1f ns per `read` call (measured with CLOCK_MONOTONIC_RAW)\n",
+           pace);
+}
+
+
+static void
+measure_write_with_gtod(void)
+{
+    size_t total = (size_t)1e6;
+
+    unsigned char *buf = malloc(1);
+    if (buf == NULL) {
+        exit(1);
+    }
+
+    struct timeval start;
+    struct timeval stop;
+
+    printf("Running %zu `write` calls...\n", total);
+    gettimeofday(&start, NULL);
+    for (unsigned int i = 0; i < total; i++) {
+        write(STDOUT_FILENO, buf, 0);
+    }
+    gettimeofday(&stop, NULL);
+
+    double pace = timeval_pace(&start, &stop, total);
+    printf("%.1f ns per `write` call (measured with `gettimeofday`)\n",
+           pace);
+}
+
+static void
+measure_write_with_cgt(void)
+{
+    size_t total = (size_t)1e6;
+
+    unsigned char *buf = malloc(1);
+    if (buf == NULL) {
+        exit(1);
+    }
+
+    struct timespec start;
+    struct timespec stop;
+
+    printf("Running %zu `write` calls...\n", total);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    for (unsigned int i = 0; i < total; i++) {
+        write(STDOUT_FILENO, buf, 0);
+    }
+    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+
+    double pace = timespec_pace(&start, &stop, total);
+    printf("%.1f ns per `write` call (measured with CLOCK_MONOTONIC_RAW)\n", pace);
+}
+
+
+int main(void)
+{
+    measure_gettimeofday();
+    measure_clock_gettime_monotonic();
+    measure_clock_gettime_monotonic_raw();
+    measure_read_with_gtod();
+    measure_read_with_cgt();
+    measure_write_with_gtod();
+    measure_write_with_cgt();
     return 0;
 }
