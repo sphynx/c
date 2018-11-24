f2e2bb57684e625066ddb1c191c5f5fb2311c400
Ivan Veselov <veselov@gmail.com>
2018-11-24 21:03:10 +0000

    Skip challenge 20, because I did challenge 19 in exactly the same way

---
+++ crypto-challenge/challenge20.c
@@ -0,0 +1,5 @@
+/*
+ * I've actually used the proposed method (i.e. breaking it
+ * statistically) for challenge 19, so it does not make much sense to
+ * repeat it here again, so I am just skipping this challenge.
+ */
